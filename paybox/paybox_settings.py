ebd20c8b769298611dda6dcc9804207170148bf7
Florent Jouatte <fjouatte@anybox.fr>
2014-10-07 10:47:52 +0200

    method to retrieve back paybox settings

--- paybox/paybox_settings.py
+++ paybox/paybox_settings.py
@@ -11,30 +11,30 @@ class PayboxSettings(osv.Model):
                 'shop_id': fields.char('Shop id'),
                 'key': fields.char('Key', password=True), }
 
-    def get_paybox_settings(self, cr, uid, ids, context=None):
+    def get_default_paybox_settings(self, cr, uid, ids, context=None):
         cfg_param = self.pool.get('ir.config_parameter')
-        site = cfg_param.get_param(cr, uid, 'paybox.site') or ''
-        rank = cfg_param.get_param(cr, uid, 'paybox.rank') or ''
-        shop_id = cfg_param.get_param(cr, uid, 'paybox.shop_id') or ''
-        key = cfg_param.get_param(cr, uid, 'paybox.key') or ''
+        site = cfg_param.get_param(cr, uid, 'paybox.site') or ""
+        rank = cfg_param.get_param(cr, uid, 'paybox.rank') or ""
+        shop_id = cfg_param.get_param(cr, uid, 'paybox.shop_id') or ""
+        key = cfg_param.get_param(cr, uid, 'paybox.key') or ""
         return {'site': site, 'rank': rank, 'shop_id': shop_id, 'key': key}
 
     def set_site(self, cr, uid, ids, context=None):
         for i in ids:
-            site = self.browse(cr, uid, i, context)["site"] or cr.dbname or ""
+            site = self.browse(cr, uid, i, context)["site"] or ""
             self.pool.get("ir.config_parameter").set_param(cr, uid, "paybox.site", site)
 
     def set_rank(self, cr, uid, ids, context=None):
         for i in ids:
-            rank = self.browse(cr, uid, i, context)["rank"] or cr.dbname or ""
+            rank = self.browse(cr, uid, i, context)["rank"] or ""
             self.pool.get("ir.config_parameter").set_param(cr, uid, "paybox.rank", rank)
 
     def set_shop_id(self, cr, uid, ids, context=None):
         for i in ids:
-            shop_id = self.browse(cr, uid, i, context)["shop_id"] or cr.dbname or ""
+            shop_id = self.browse(cr, uid, i, context)["shop_id"] or ""
             self.pool.get("ir.config_parameter").set_param(cr, uid, "paybox.shop_id", shop_id)
 
     def set_key(self, cr, uid, ids, context=None):
         for i in ids:
-            key = self.browse(cr, uid, i, context)["key"] or cr.dbname or ""
+            key = self.browse(cr, uid, i, context)["key"] or ""
             self.pool.get("ir.config_parameter").set_param(cr, uid, "paybox.key", key)
